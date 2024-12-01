0103c1181b48c92f9508b02a487455465e2882c8
Sheane <Sheane@MSI>
2024-12-01 16:31:36 +0800

    Start

---
+++ src/main/java/com/mycompany/online_ticket_booking_system/Online_Ticket_Booking_System.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.online_ticket_booking_system;
+
+/**
+ *
+ * @author Sheane
+ */
+public class Online_Ticket_Booking_System {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
